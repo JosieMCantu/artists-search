e3519f997ea5240ad9e8113d2a96cadfbeda488f
Stephen Leisy <stephenleisy@Stephens-MacBook-Pro.local>
2021-05-26 16:46:35 -0700

    added api file

--- src/components/songs/Song.jsx
+++ src/components/songs/Song.jsx
@@ -1,24 +1,16 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-
-export const getSongsById = async (releaseId) => {
-    const res = await fetch(`http://musicbrainz.org/ws/2/recording?release=${releaseId}&fmt=json`);
-    const {recordings} = await res.json();
-    return recordings;
-
-} 
-function Song({title}) {
-    return (
-        <div>
-            <p>{title}</p>
-        </div>
-    )
+function Song({ title }) {
+  return (
+    <div>
+      <p>{title}</p>
+    </div>
+  );
 }
 
 Song.propTypes = {
-    // releaseId: PropTypes.string.isRequired,
-}
+  // releaseId: PropTypes.string.isRequired,
+};
 
-export default Song
-
+export default Song;
--- src/containers/LyricsContainer.jsx
+++ src/containers/LyricsContainer.jsx
@@ -1,28 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import Lyrics from '../components/lyrics/Lyrics'; 
-import { fetchLyrics }  from '../LyricService/LyricApi'; 
-
+import Lyrics from '../components/lyrics/Lyrics';
+import { fetchLyrics } from '../services/apiUtils';
 
 const LyricsContainer = () => {
-  const [loading, setLoading] = useState(true); 
-  const [lyrics, setLyrics] = useState(''); 
+  const [loading, setLoading] = useState(true);
+  const [lyrics, setLyrics] = useState('');
 
   const { artistName, title } = useParams();
 
-  useEffect (() => {
+  useEffect(() => {
     fetchLyrics(artistName, title)
       .then(setLyrics)
-      .finally(() => setLoading(false)); 
+      .finally(() => setLoading(false));
   }, []);
 
-  if(loading) return <h1>Loading...</h1>; 
+  if (loading) return <h1>Loading...</h1>;
 
   return (
     <div>
-      <Lyrics lyrics={lyrics}/>
+      <Lyrics lyrics={lyrics} />
     </div>
   );
 };
 
-export default LyricsContainer; 
+export default LyricsContainer;
