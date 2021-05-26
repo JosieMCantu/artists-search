32e51dff705ed2f1320dce4b94d2404399a5159d
Stephen Leisy <stephenleisy@Stephens-MacBook-Pro.local>
2021-05-26 14:09:24 -0700

    finished default image if image link is broken.

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchPageContainer from '../../containers/SearchPageContainer';
 import ArtistContainer from '../../containers/ArtistContainer';
 import SongsContainer from '../../containers/SongsContainer';
@@ -8,12 +8,12 @@ import LyricsContainer from '../../containers/LyricsContainer';
 export default function App() {
   return (
     <Router>
-    <Switch>
-      <Route exact path='/' component={SearchPageContainer}/>
-      <Route exact path='/:id' component={ArtistContainer}/>
-      <Route exact path='/albums/:id' component={SongsContainer}/>
-      <Route exact path='/song/:artist/:title' component={LyricsContainer}/>
-    </Switch>
+      <Switch>
+        <Route exact path="/" component={SearchPageContainer} />
+        <Route exact path="/:id" component={ArtistContainer} />
+        <Route exact path="/albums/:id" component={SongsContainer} />
+        <Route exact path="/song/:artist/:title" component={LyricsContainer} />
+      </Switch>
     </Router>
   );
 }
--- src/components/artist/Album.jsx
+++ src/components/artist/Album.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Album({ title, date, id }) {
+  const addDefaultSrc = (e) => {
+    e.target.src =
+      // eslint-disable-next-line max-len
+      'https://table9mutant.files.wordpress.com/2016/01/img_5688.jpeg?w=480&h=360';
+  };
   return (
     <>
       <h2>{title}</h2>
       <img
         src={`http://coverartarchive.org/release/${id}/front`}
+        onError={addDefaultSrc}
         alt="sorry! No album artwork."
         height="300px"
         width="300px"
